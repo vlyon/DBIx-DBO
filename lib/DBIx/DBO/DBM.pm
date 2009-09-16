9d06bc0984f57d4bd6291bf659a6efea53dd72ad
Vernon <vlyon@users.noreply.github.com>
2009-09-16 09:31:16 +0000

    Fixing DBIx::DBO::DBM

--- lib/DBIx/DBO/DBM.pm
+++ lib/DBIx/DBO/DBM.pm
@@ -15,21 +15,37 @@ sub _like_to_regex {
 
 sub _get_table_schema {
     my $me = shift;
-    my $schema = my $q_schema = shift;
+    my $schema = shift; # Not used
     my $table = my $q_table = shift;
     DBIx::DBO::ouch 'No table name supplied' unless defined $table and length $table;
 
-    $q_schema =~ s/([\\_%])/\\$1/g if defined $q_schema;
-    $q_table =~ s/([\\_%])/\\$1/g;
+    return '';
+}
 
-    my $info = $me->rdbh->table_info(undef, $q_schema, $q_table)->fetchall_arrayref;
-use Data::Dumper;
-warn 'info', substr Dumper($info), 5;
-my $dbh = { map {$_ => $me->rdbh->{$_}} keys %{$me->rdbh->{dbm_valid_attrs}} };
-warn 'dbh', substr Dumper($dbh), 5;
-    DBIx::DBO::ouch 'Invalid table: '.$table
-        unless $info and @$info == 1 and $info->[0][2] eq $table;
-    return $info->[0][1];
+sub _get_table_info {
+    my $me = shift;
+    my $schema = shift; # Not used
+    my $table = my $q_table = shift;
+    DBIx::DBO::ouch 'No table name supplied' unless defined $table and length $table;
+
+    unless (exists $me->rdbh->{dbm_tables}{$q_table}
+            and exists $me->rdbh->{dbm_tables}{$q_table}{cols}
+            and ref $me->rdbh->{dbm_tables}{$q_table}{cols} eq 'ARRAY') {
+        $q_table = $me->_qi($table); # Try with the qouted table name
+        unless (exists $me->rdbh->{dbm_tables}{$q_table}
+                and exists $me->rdbh->{dbm_tables}{$q_table}{cols}
+                and ref $me->rdbh->{dbm_tables}{$q_table}{cols} eq 'ARRAY') {
+            DBIx::DBO::ouch 'Invalid table: '.$q_table;
+        }
+    }
+
+    my %h;
+    my $i;
+    for my $col (@{$me->rdbh->{dbm_tables}{$q_table}{cols}}) {
+        $h{Fields}{$col} = ++$i;
+    }
+    $h{PrimaryKeys} = $me->rdbh->{dbm_tables}{$q_table}{cols};
+    $me->{TableInfo}{$schema}{$table} = \%h;
 }
 
 1;
