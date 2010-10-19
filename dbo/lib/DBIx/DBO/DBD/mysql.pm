ee0d252d882530440a31d19b88b48f773c6a3e5d
vlyon <vlyon@9fb3e3b8-e216-4c00-9ae5-ae3151f544c8>
2010-10-19 14:31:02 +0000

    Fix checking of aliased columns in MySQL

--- dbo/lib/DBIx/DBO/DBD/mysql.pm
+++ dbo/lib/DBIx/DBO/DBD/mysql.pm
@@ -65,6 +65,14 @@ package # hide from PAUSE
     DBIx::DBO::Query::DBD::mysql;
 use DBIx::DBO::Common;
 
+# MySQL doesn't allow the use of aliases in the WHERE clause
+sub _alias_preference {
+    my $me = shift;
+    my $method = shift;
+    return 0 if $method eq 'join_on' or $method eq 'where';
+    return 1;
+}
+
 sub found_rows {
     my $me = shift;
     my $sql = $me->sql;
