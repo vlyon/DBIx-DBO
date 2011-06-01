f2b62fc439b89ba358d74e8928426d12f22cd70f
vlyon <vlyon@9fb3e3b8-e216-4c00-9ae5-ae3151f544c8>
2011-06-01 11:11:12 +0000

    Change the Query sth to _sth. It should be a private method.

--- dbo/lib/DBIx/DBO/DBD/mysql.pm
+++ dbo/lib/DBIx/DBO/DBD/mysql.pm
@@ -75,7 +75,7 @@ sub found_rows {
     my $me = shift;
     my $sql = $me->sql;
     if (not defined $me->{Found_Rows} and $sql =~ / SQL_CALC_FOUND_ROWS /) {
-        $me->run unless $me->sth->{Executed};
+        $me->run unless $me->_sth->{Executed};
         ($me->{Found_Rows}) = $me->rdbh->selectrow_array('SELECT FOUND_ROWS()');
     }
     $me->{Found_Rows};
