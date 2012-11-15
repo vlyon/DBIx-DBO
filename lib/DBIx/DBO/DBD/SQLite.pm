ae8872f28a37b23b4b98cc976141be02d0b6a580
Vernon <vlyon@users.noreply.github.com>
2012-11-15 13:33:49 +0000

    Remove support for older DBD::SQLite

--- lib/DBIx/DBO/DBD/SQLite.pm
+++ lib/DBIx/DBO/DBD/SQLite.pm
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use DBD::SQLite 1.27;
+use DBD::SQLite 1.31;
 
 package # hide from PAUSE
     DBIx::DBO::DBD::SQLite;
@@ -19,15 +19,6 @@ sub _get_table_schema {
         'TABLE,VIEW,GLOBAL TEMPORARY,LOCAL TEMPORARY,SYSTEM TABLE', {Escape => '\\'})->fetchall_arrayref;
     croak 'Invalid table: '.$class->_qi($me, $table) unless $info and @$info == 1 and $info->[0][2] eq $table;
     return $info->[0][1];
-}
-
-# Hack to fix quoted primary keys
-if (eval "$DBD::SQLite::VERSION < 1.30") {
-    *_set_table_key_info = sub {
-        my($class, $me, $schema, $table, $h) = @_;
-        $class->SUPER::_set_table_key_info($me, $schema, $table, $h);
-        s/^(["'`])(.+)\1$/$2/ for @{$h->{PrimaryKeys}}; # dequote
-    };
 }
 
 sub _save_last_insert_id {
