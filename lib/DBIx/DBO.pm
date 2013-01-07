72ae3230b25cd4ce82077d212e5e887c8bd336c7
Vernon <vlyon@users.noreply.github.com>
2013-01-07 12:41:06 +0000

    Release 0.33

--- lib/DBIx/DBO.pm
+++ lib/DBIx/DBO.pm
@@ -17,7 +17,7 @@ my $need_c3_initialize;
 my @ConnectArgs;
 
 BEGIN {
-    $VERSION = '0.32';
+    $VERSION = '0.33';
     # The C3 method resolution order is required.
     if ($] < 5.009_005) {
         require MRO::Compat;
